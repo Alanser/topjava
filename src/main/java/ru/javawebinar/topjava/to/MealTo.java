d0332f3275239fe22d9dd41c14d38deb7203b98f
Anton Alputov <alputov@gmail.com>
2019-12-11 19:39:38 +0600

    HW9. Fix

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,22 +1,15 @@
 package ru.javawebinar.topjava.to;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class MealTo extends BaseTo {
 
-    @NotNull
     private LocalDateTime dateTime;
 
-    @NotBlank
-    @Size(min = 2, max = 100)
     private String description;
 
-    @NotNull
     private int calories;
 
     private boolean excess;
